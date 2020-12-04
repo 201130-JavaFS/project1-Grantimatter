45d53087f626035bc30c0a8e3ce443885e8a144a
Grant Wiswell <wiswellgrant@gmail.com>
2020-12-04 16:04:21 -0600

    Project setup with dependencies and removed uneccessary files

---
+++ src/main/java/com/revature/TestMain.java
@@ -0,0 +1,10 @@
+package com.revature;
+
+import org.apache.log4j.Logger;
+
+public class TestMain {
+    static Logger log = Logger.getLogger(TestMain.class);
+    public static void main(String[] args) {
+        log.info("Hello World!");
+    }
+}
