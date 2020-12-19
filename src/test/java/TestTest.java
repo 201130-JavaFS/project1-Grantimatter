c9f435f5bad30d797fab8fccf968ea944068237b
Grant Wiswell <wiswellgrant@gmail.com>
2020-12-19 15:37:57 -0600

    Added tests so Jenkins mihgt use them

---
+++ src/test/java/TestTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+public class TestTest {
+
+    static Logger log = LogManager.getLogger(TestTest.class);
+
+    @Test
+    public void runThis(){
+        log.info("I'm working!");
+    }
+}
