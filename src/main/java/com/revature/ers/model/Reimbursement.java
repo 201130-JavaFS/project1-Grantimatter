3e72036ee4befc4685fe26511450a7958a7604a2
Grant Wiswell <wiswellgrant@gmail.com>
2020-12-21 23:54:07 -0600

    Eidited reimbursement query to be prettier

--- src/main/java/com/revature/ers/model/Reimbursement.java
+++ src/main/java/com/revature/ers/model/Reimbursement.java
@@ -13,10 +13,10 @@ public class Reimbursement {
     @Id
     int id;
     BigDecimal amount;
-    int author_id;
-    int resolver_id;
-    int status_id;
-    int type_id;
+    String author;
+    String resolver;
+    String status;
+    String type;
     String description;
     Date submitted;
     Date resolved;
@@ -29,35 +29,35 @@ public class Reimbursement {
         this.id = id;
     }
 
-    public Reimbursement(BigDecimal amount, int author_id, int type_id) {
+    public Reimbursement(BigDecimal amount, String author, String type) {
         this.amount = amount;
-        this.author_id = author_id;
-        this.type_id = type_id;
+        this.author = author;
+        this.type = type;
     }
 
-    public Reimbursement(BigDecimal amount, int author_id, int type_id, String description) {
-        this(amount, author_id, type_id);
+    public Reimbursement(BigDecimal amount, String author, String type, String description) {
+        this(amount, author, type);
         this.description = description;
     }
 
-    public Reimbursement(int id, BigDecimal amount, int author_id, int type_id, int status_id, Timestamp submitted) {
-        this(amount, author_id, type_id);
+    public Reimbursement(int id, BigDecimal amount, String author, String type, String status, Timestamp submitted) {
+        this(amount, author, type);
         this.id = id;
-        this.status_id = status_id;
+        this.status = status;
         this.submitted = submitted;
     }
 
-    public Reimbursement(int id, BigDecimal amount, int author_id, int type_id, String description, int status_id, Timestamp submitted) {
-        this(amount, author_id, type_id);
+    public Reimbursement(int id, BigDecimal amount, String author, String type, String description, String status, Timestamp submitted) {
+        this(amount, author, type);
         this.id = id;
-        this.status_id = status_id;
+        this.status = status;
         this.description = description;
         this.submitted = submitted;
     }
 
-    public Reimbursement(int id, BigDecimal amount, int author_id, int resolver_id, int type_id, int status_id, String description, Timestamp submitted, Timestamp resolved) {
-        this(id, amount, author_id, type_id, description, status_id, submitted);
-        this.resolver_id = resolver_id;
+    public Reimbursement(int id, BigDecimal amount, String author, String resolver, String type, String status, String description, Timestamp submitted, Timestamp resolved) {
+        this(id, amount, author, type, description, status, submitted);
+        this.resolver = resolver;
         this.resolved = resolved;
     }
 
@@ -77,28 +77,28 @@ public class Reimbursement {
         this.amount = amount;
     }
 
-    public int getAuthor_id() {
-        return author_id;
+    public String getAuthor() {
+        return author;
     }
 
-    public void setAuthor_id(int author_id) {
-        this.author_id = author_id;
+    public void setAuthor(String author) {
+        this.author = author;
     }
 
-    public int getResolver_id() {
-        return resolver_id;
+    public String getResolver() {
+        return resolver;
     }
 
-    public void setResolver_id(int resolver_id) {
-        this.resolver_id = resolver_id;
+    public void setResolver(String resolver) {
+        this.resolver = resolver;
     }
 
-    public int getStatus_id() {
-        return status_id;
+    public String getStatus() {
+        return status;
     }
 
-    public void setStatus_id(int status_id) {
-        this.status_id = status_id;
+    public void setStatus(String status) {
+        this.status = status;
     }
 
     public String getDescription() {
@@ -125,12 +125,12 @@ public class Reimbursement {
         this.resolved = resolved;
     }
 
-    public int getType_id() {
-        return type_id;
+    public String getType() {
+        return type;
     }
 
-    public void setType(int type_id) {
-        this.type_id = type_id;
+    public void setType(String type) {
+        this.type = type;
     }
 
     @Override
@@ -138,26 +138,26 @@ public class Reimbursement {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Reimbursement that = (Reimbursement) o;
-        return id == that.id && author_id == that.author_id && resolver_id == that.resolver_id && status_id == that.status_id && type_id == that.type_id && Objects.equals(amount, that.amount) && Objects.equals(description, that.description) && Objects.equals(submitted, that.submitted) && Objects.equals(resolved, that.resolved);
+        return id == that.id && Objects.equals(amount, that.amount) && Objects.equals(author, that.author) && Objects.equals(resolver, that.resolver) && Objects.equals(status, that.status) && Objects.equals(type, that.type) && Objects.equals(description, that.description) && Objects.equals(submitted, that.submitted) && Objects.equals(resolved, that.resolved);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, amount, author_id, resolver_id, status_id, type_id, description, submitted, resolved);
+        return Objects.hash(id, amount, author, resolver, status, type, description, submitted, resolved);
     }
 
     @Override
     public String toString() {
-        return "{" +
-                "\"id\"=" + id +
-                ", \"amount\"=" + amount +
-                ", \"author_id\"=" + author_id +
-                ", \"resolver_id\"=" + resolver_id +
-                ", \"type_id\"=" + type_id +
-                ", \"status\"=" + status_id +
-                ", \"description\"=" + description +
-                ", \"submitted\"=\"" + submitted +"\"" +
-                ", \"resolved\"=" + resolved +
-                "}";
+        return "Reimbursement{" +
+                "id=" + id +
+                ", amount=" + amount +
+                ", author='" + author + '\'' +
+                ", resolver='" + resolver + '\'' +
+                ", status='" + status + '\'' +
+                ", type='" + type + '\'' +
+                ", description='" + description + '\'' +
+                ", submitted=" + submitted +
+                ", resolved=" + resolved +
+                '}';
     }
 }
